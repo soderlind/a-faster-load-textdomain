a48ecc7fad4b5647f73c4b7919a0cfd540f74797
Per Søderlind <per@soderlind.no>
2023-08-10 22:52:46 +0200

    Refactor code, instead of using a transient, save .mo file as PHP array, and include the array instead of the .mo file.

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Plugin Name ===
 Contributors: PerS
 Tags: l10n, load_textdomain, cache, transient
-Requires at least: 6.3
+Requires at least: 5.9
 Tested up to: 6.3
-Stable tag: 1.0.3
+Stable tag: 2.0.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -11,26 +11,19 @@ A faster load_textdomain
 
 == Description ==
 
-This plugin caches the .mo file in a transient to speed up the load_textdomain function in WordPress. 
-You don't have to, but you should add an an object cache.
+This is a WordPress plugin that caches the .mo file as PHP array, and [include](https://www.php.net/manual/en/function.include.php) the array instead of the .mo file.
+In theory, nothing is faster in PHP than loading and executing another PHP file.
 
 
 == Installation ==
 
-1. Upload the plugin files to the `/wp-content/plugins/mu-plugind` directory.
-2. Optional, add an object cache to your WordPress installation.
-
-== Frequently Asked Questions ==
-
-= How do I add an object cache? =
-
-You can add an object cache by installing a caching plugin like 
-* [W3 Total Cache](https://wordpress.org/plugins/w3-total-cache/)
-* [Redis Object Cache](https://wordpress.org/plugins/redis-cache/)
-* [Object Cache Pro](https://objectcache.pro/) 
+1. Download the plugin files and extract `wp-cache-textdomain.php` to the `wp-content/mu-plugins` directory.
 
 
 == Changelog ==
+
+= 2.0.0 =
+* Refactor code, instead of using a transient, save .mo file as PHP array, and [include](https://www.php.net/manual/en/function.include.php) the array instead of the .mo file.
 
 = 1.0.3 =
 * Housekeeping.
--- wp-cache-textdomain.php
+++ wp-cache-textdomain.php
@@ -1,20 +1,58 @@
 <?php
 /**
  * Plugin Name: A faster load_textdomain
- * Version: 1.0.3
- * Description: Cache the .mo file in a transient
+ * Version: 2.0.0
+ * Description: Cache the .mo file as PHP array, and load the array instead of the .mo file.
  * Author: Per Soderlind
  * Author URI: https://soderlind.no
  * Plugin URI: https://github.com/soderlind/wp-cache-textdomain
  * License: GPLv2 or later
- * Credit: nicofuma , I just created the plugin based on his patch (https://core.trac.wordpress.org/ticket/32052)
- * Save the plugin in mu-plugins. You don't have to, but you should add an an object cache.
  *
  * @package wp-cache-textdomain
  */
 
 declare( strict_types = 1 );
 namespace Soderlind\Plugin\WP_Cache_Textdomain;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WP_Filesystem' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/file.php';
+}
+
+/**
+ * Translation_Entry class.
+ *
+ * @package wp-cache-textdomain
+ */
+class Translation_Entry extends \Translation_Entry {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $args {
+	 *     Optional. Array of arguments for the translation entry.
+	 *
+	 *     @type string $singular Singular form of the string.
+	 *     @type string $plural   Plural form of the string.
+	 *     @type string $context  Context information for the translators.
+	 *     @type string $domain   Text domain. Unique identifier for retrieving translated strings.
+	 *     @type string $context  Context information for the translators.
+	 *     @type string $translations {
+	 *         Optional. Array of translations for different plural forms.
+	 *
+	 *         @type string $singular Singular form of the string.
+	 *         @type string $plural   Plural form of the string.
+	 *     }
+	 * }
+	 */
+	public static function __set_state( $args ) {
+		return new \Translation_Entry( $args );
+	}
+}
+
 
 /**
  * Load a text domain faster by caching the parsed .mo file data.
@@ -28,56 +66,76 @@ namespace Soderlind\Plugin\WP_Cache_Textdomain;
  */
 function a_faster_load_textdomain( $loaded, $domain, $mofile, $locale = null ) {
 	global $l10n;
-
-	// Create the hash.
-	$hash = md5_file( $mofile ); // Will return false if the file doesn't exist.
-	if ( false === $hash ) {
-		// If the file is not readable, return false.
+	if ( ! is_readable( $mofile ) ) {
 		return false;
 	}
 
-	// Check if the data for the file is already in the transient cache.
-	$data  = ( ( \is_multisite() ) ) ? \get_site_transient( $hash ) : \get_transient( $hash );
+	$cache_path = WP_CONTENT_DIR . '/cache/wp-cache-textdomain';
+	if ( ! file_exists( $cache_path ) ) {
+		mkdir( $cache_path, 0770, true );
+	}
+	$cache_file = sprintf( '%s/mo-%s.php', $cache_path, md5( $mofile ) );
+
+	if ( file_exists( $cache_file ) ) {
+		include $cache_file;
+		$data = isset( $val ) ? $val : false;
+	} else {
+		$data = false;
+	}
+
 	$mtime = filemtime( $mofile );
 
-	// Create a new MO object.
 	$mo = new \MO();
 
-	// Check if the data is already in the cache and if it is up-to-date.
+	// Check if $data is empty or if the mtime of the file is greater than the mtime in $data.
 	if ( ! $data || ! isset( $data['mtime'] ) || $mtime > $data['mtime'] ) {
-		// If the data is not in the cache or is outdated, import the .mo file into the MO object.
+		// Import the translations from the MO file.
 		if ( ! $mo->import_from_file( $mofile ) ) {
-			// If the import fails, return false.
 			return false;
 		}
 
-		// Store the entries and headers from the MO object in an array with the modification time.
+		// Create an array with the mtime, file, entries, and headers.
 		$data = [
 			'mtime'   => $mtime,
+			'file'    => $mofile,
 			'entries' => $mo->entries,
 			'headers' => $mo->headers,
 		];
 
-		// Store the data in a transient with the MD5 hash of the .mo file path as the key.
-		if ( \is_multisite() ) {
-			\set_site_transient( $hash, $data );
+		// Export the data to a PHP file.
+		$val = var_export( $data, true ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_export
+
+		// Replace Translation_Entry with Lynt_Translation_Entry.
+		$val = str_replace( 'Translation_Entry::', '\Soderlind\Plugin\WP_Cache_Textdomain\Translation_Entry::', $val );
+
+		// Write the data to the cache file using WP_Filesystem if available, otherwise use file_put_contents.
+		if ( ! WP_Filesystem() ) {
+			file_put_contents( $cache_file, '<?php $val = ' . $val . ';', LOCK_EX ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_read_file_put_contents
 		} else {
-			\set_transient( $hash, $data );
+			global $wp_filesystem;
+			$wp_filesystem->put_contents( $cache_file, '<?php $val = ' . $val . ';', FS_CHMOD_FILE );
 		}
 	} else {
-		// If the data is already in the cache and the file has not been modified, retrieve the data from the cache.
+		// If the data is already cached, use it.
 		$mo->entries = $data['entries'];
 		$mo->headers = $data['headers'];
 	}
 
-	// If the text domain already exists in the $l10n global variable, merge the new MO object with the existing object.
+	// Merge the translations with the existing translations for the domain.
 	if ( isset( $l10n[ $domain ] ) ) {
 		$mo->merge_with( $l10n[ $domain ] );
 	}
 
-	// Set the $l10n global variable to the new MO object and return true.
+	// Set the translations for the domain.
 	$l10n[ $domain ] = &$mo; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 
+	// Return true to indicate success.
 	return true;
 }
-\add_filter( 'pre_load_textdomain', __NAMESPACE__ . '\a_faster_load_textdomain', 1, 4 );
+
+
+if ( version_compare( $GLOBALS['wp_version'], '6.3' ) >= 0 ) {
+	\add_filter( 'pre_load_textdomain', __NAMESPACE__ . '\a_faster_load_textdomain', 1, 4 );
+} else {
+	\add_filter( 'override_load_textdomain', __NAMESPACE__ . '\a_faster_load_textdomain', 0, 3 );
+}
